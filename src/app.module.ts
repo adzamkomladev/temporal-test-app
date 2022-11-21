069839991b03fa373fd8e71f5c3353ce77c89f25
Komla Adzam <adzamkomla.dev@gmail.com>
2022-11-21 14:11:29 +0000

    push updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,28 @@ import {
   bundleWorkflowCode,
   NativeConnection,
   Runtime,
+  DefaultLogger
 } from '@temporalio/worker';
 
 import { AppService } from './app.service';
 
 import { AppController } from './app.controller';
 
+const logger = new DefaultLogger('DEBUG', ({ level, message }) => {
+  console.log(`Custom logger: ${level} — ${message}`);
+});
+
 @Module({
   imports: [TemporalModule.forRootAsync({
     useFactory: async () => {
-      Runtime.install({});
+      Runtime.install({logger});
       
       const connection = await NativeConnection.connect({
         address: 'localhost:7233',
       });
       const workflowBundle = await bundleWorkflowCode({
         workflowsPath: path.join(__dirname, 'app.workflow.js'),
+
       });
 
       return {
